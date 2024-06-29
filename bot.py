41fec117c5cfd9f51b21582546d22a8ac4d763f3
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-06-29 11:29:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # (c) @noob_savvy
 
 import os
+import pyrogram
 import asyncio
 import traceback
 from binascii import (
@@ -120,7 +121,7 @@ async def main(bot: Client, message: Message):
                 return
 
         if message.from_user.id in Config.BANNED_USERS:
-            await message.reply_text("Sorry, You are banned!\n\nContact [Support Group](https://t.me/JoinOT)",
+            await message.reply_text("Sorry, You are banned!\n\nContact [Support Group](https://t.me/Savvy_robot_support)",
                                      disable_web_page_preview=True)
             return
 
@@ -188,7 +189,43 @@ async def sts(_, m: Message):
         quote=True
     )
 
+#-------------AUTO_CAPTION_FEATURE------------------#
+@Bot.on_message(pyrogram.filters.channel)
+def edit_caption(bot, update: pyrogram.types.Message):
+  if (config.custom_caption):
+      motech, _ = get_file_details(update)
+      try:
+          try: update.edit(custom_caption.format(file_name=motech.file_name))
+          except pyrogram.errors.FloodWait as FloodWait:
+              asyncio.sleep(FloodWait.value)
+              update.edit(custom_caption.format(file_name=motech.file_name, mote))
+      except pyrogram.errors.MessageNotModified: pass 
+  else:
+      return
 
+def get_file_details(update: pyrogram.types.Message):
+  if update.media:
+    for message_type in (
+        "photo",
+        "animation",
+        "audio",
+        "document",
+        "video",
+        "video_note",
+        "voice",
+        # "contact",
+        # "dice",
+        # "poll",
+        # "location",
+        # "venue",
+        "sticker"
+    ):
+        obj = getattr(update, message_type)
+        if obj:
+            return obj, obj.file_id
+
+
+#________________________________________________________________________________#
 @Bot.on_message(filters.private & filters.command("ban_user") & filters.user(Config.BOT_OWNER))
 async def ban(c: Client, m: Message):
     
